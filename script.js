7afd359777c68c975c9072a7179797ce1040a8f1
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-10-04 06:18:32 +0700

    Footer and 2.html 3.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const personalities = [
     {'fx': 'BBBB', 'url': '1.html', 'label': 'Idealis Penyelaras'},
-    {'fx': 'ABBB', 'url': '2.html', 'label': 'Idealis Penyelaras'},
+    {'fx': 'ABBB', 'url': '2.html', 'label': 'Idealis Terlibat'},
     {'fx': 'ABAB', 'url': '3.html', 'label': 'Pemikir Dinamis'},
     {'fx': 'ABAA', 'url': '4.html', 'label': 'Pemikir Pendobrak'},
     {'fx': 'ABBA', 'url': '5.html', 'label': 'Idealis Spontan'},
@@ -45,6 +45,14 @@ function displayDropdownTopMenu() {
 
     const personalityContainer = document.querySelector(".personality-container");
     personalityContainer.innerHTML = personalityHTML;
+}
+
+function displayFooter() {
+    let footerHTML = '<p>© iPersonic <a href="#">Personality Test</a> <a href="#">Career Test</a> <a href="#">Persönlichkeitstest</a> <a href="#">Berufstest</a> <a href="#">Privacy Policy</a> <a href="#">Felicitas Heyne</a> <a href="#">Impressum</a> <a href="#">Help & Contact</a> iPersonic supports <a href="#">Audiopedia Foundation</a></p>';
+    footerHTML += '<p><a href="#">English</a> | <a href="#">Deutsch</a> | <a href="#">Español</a> | <a href="#">Français</a> | <a href="#">Português</a> | <a href="#">中文</a> | <a href="#">Русский</a> | <a href="#">हिंदी</a> | <a href="#">العربية</a> | <a href="#">日本語</a> | <a href="#">Bahasa Indonesia</a> | <a href="#">Italiano</a> | <a href="#">Svenska</a> | <a href="#">Nederlands</a> | <a href="#">עברית</a> | <a href="#">Afrikaans</a> | <a href="#">Bosanski</a> | <a href="#">Català</a> | <a href="#">Čeština</a> | <a href="#">Hrvatski</a> | <a href="#">Magyar</a> | <a href="#">Română</a> | <a href="#">Slovenski</a> | <a href="#">Slovenský</a> | <a href="#">Srpski</a> | <a href="#">Български</a></p>';
+
+    const footerContainer = document.querySelector(".footer-container");
+    footerContainer.innerHTML = footerHTML;
 }
 
 function displayQuizOptions(fxAnswer) {
@@ -124,4 +132,5 @@ function logicQuizOptions() {
 
 // call functions
 displayDropdownTopMenu();
+displayFooter();
 logicQuizOptions();
